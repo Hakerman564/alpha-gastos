39e4e56156a04d06283cf8efdef5534ea73e0e66
negel <elbueno000sin@gmail.com>
2024-01-10 11:51:44 -0400

    intalacion de archivos

---
+++ Gastos-pagos-alpha/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Gastos_pagos_alpha.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
